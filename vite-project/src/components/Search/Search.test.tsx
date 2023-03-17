17bbeac4239c83d3c8c3ee9324482729b9bfd728
oolenkazolot <oolenka.zolot@gmail.com>
2023-03-17 16:16:44 +0100

    feat: create PageTitle.test.tsx

--- vite-project/src/components/Search/Search.test.tsx
+++ vite-project/src/components/Search/Search.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Search } from '../Search/Search';
 
 describe('Search', () => {
@@ -7,5 +7,18 @@ describe('Search', () => {
     expect(screen.getByRole('textbox')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Search text...')).toBeInTheDocument();
     expect(screen.getByDisplayValue(localStorage.getItem('searchValue') || '')).toBeInTheDocument();
+  });
+});
+
+describe('Search', () => {
+  it('render Search component', () => {
+    screen.debug();
+    render(<Search />);
+    expect(screen.getByDisplayValue(localStorage.getItem('searchValue') || '')).toBeInTheDocument();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'React' },
+    });
+    expect(screen.getByDisplayValue(/React/i)).toBeInTheDocument();
+    screen.debug();
   });
 });
